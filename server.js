4d5caa533039fc40f797b003c54dc61505f22509
Rashid Zainullov <rashid.zainullov@gmail.com>
2022-09-17 23:15:01 +0400

    hotfix

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = 3000
 app.use(express.static('dist'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`_homer is listening on port ${port}`)
 })
